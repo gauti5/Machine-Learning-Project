170d4048010f1cdcaae4be2b472c3b84e8803488
Sandeep <sndpgauti@gmail.com>
2024-05-01 14:35:12 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -17,14 +17,14 @@ def get_requirements_list()->List[str]:
     mention in the requirements.txt file
     '''
     with open(REQUIREMENTS) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
 
